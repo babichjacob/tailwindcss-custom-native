010c86db00c867579bfb8261e5a59614e16e7daa
Jacob Babich <jacobbabichpublic+git@gmail.com>
2019-09-13 00:44:38 -0400

    ⚡️ feat: configure multiple custom utilities in a single function call
    The `customNative` function is now variadic, so it can accept multiple configurations, meaning that what used to be written as:
    
    ```js
    plugins: [customNative(config1), customNative(config2), customNative(config3)]
    ```
    
    can now be written as:
    
    ```js
    plugins: [customNative(config1, config2, config3)
    ```
    
    The documentation has been updated to reflect this.

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
-const { kebabCase } = require('lodash');
+const { kebabCase } = require("lodash");
 
-module.exports = function({ key, property, rename }, addUtilitiesOptions={}) {
-	// E.x. 'mixBlendMode' -> 'mix-blend-mode'
-	const keyHyphenated = kebabCase(key);
-	
-	if (property === undefined) {
-		property = keyHyphenated;
-	}
-	
-	if (rename === undefined) {
-		rename = keyHyphenated;
-	}
-	
-	return ({addUtilities, e, theme, variants }) => {
-		const newUtilities = {};
-		
-		for (const [name, value] of Object.entries(theme(key, {}))) {
-			const className = rename === '' ? `.${e(`${name}`)}`: `.${e(`${rename}-${name}`)}`;
-			
-			newUtilities[className] = {
-				[property]: value,
-			};
-		}
-		
-		const specifiedVariants = variants(key, []);
-		
-		// This means a `variants` key in `addUtilitiesOptions` will override `specifiedVariants`!
-		addUtilities(newUtilities, {variants: specifiedVariants, ...addUtilitiesOptions});
-	};
+function single({ key, property, rename, addUtilitiesOptions }) {
+  // E.x. 'mixBlendMode' -> 'mix-blend-mode'
+  const keyHyphenated = kebabCase(key);
+
+  if (property === undefined) {
+    property = keyHyphenated;
+  }
+
+  if (rename === undefined) {
+    rename = keyHyphenated;
+  }
+
+  if (addUtilitiesOptions === undefined) {
+    addUtilitiesOptions = {};
+  }
+
+  return ({ addUtilities, e, theme, variants }) => {
+    const newUtilities = {};
+
+    for (const [name, value] of Object.entries(theme(key, {}))) {
+      const className = rename === "" ? `.${e(`${name}`)}` : `.${e(`${rename}-${name}`)}`;
+
+      newUtilities[className] = {
+        [property]: value
+      };
+    }
+
+    const specifiedVariants = variants(key, []);
+
+    // This means a `variants` key in `addUtilitiesOptions` will override `specifiedVariants`!
+    addUtilities(newUtilities, { variants: specifiedVariants, ...addUtilitiesOptions });
+  };
+}
+
+function multiple(...configurations) {
+  return pluginHelpers => {
+    configurations.forEach(configuration => single(configuration)(pluginHelpers));
+  };
+}
+
+module.exports = function(...args) {
+  // Determine which form of arguments is being used
+  if (args.length === 0) {
+    console.warn(
+      "customNative was called without any arguments; this can be fixed by passing at least one argument of the form { key, property, rename, addUtilitiesOptions }"
+    );
+  } else if (args.length === 2 && !args[1].hasOwnProperty("key")) {
+    // The old API is being used
+    return single({ ...args[0], addUtilitiesOptions: args[1] });
+  } else {
+    return multiple(...args);
+  }
 };
