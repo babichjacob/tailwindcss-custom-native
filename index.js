ce5ebb76e0568873ed3948d4ae2993fc92b21c8b
Navith <navithnavithnavith+git@gmail.com>
2020-03-07 18:30:18 -0500

    feat+chore+style: more warnings for incorrect configuration, version bump and change tailwindcss to a peer dependency, and refactor `index.js`

--- index.js
+++ index.js
@@ -1,55 +1,45 @@
 const { kebabCase } = require("lodash");
 
-function single({ key, property, rename, addUtilitiesOptions }) {
+const single = ({ key, property, rename, addUtilitiesOptions = {} }) => {
   // E.x. 'mixBlendMode' -> 'mix-blend-mode'
   const keyHyphenated = kebabCase(key);
 
-  if (property === undefined) {
-    property = keyHyphenated;
-  }
-
-  if (rename === undefined) {
-    rename = keyHyphenated;
-  }
-
-  if (addUtilitiesOptions === undefined) {
-    addUtilitiesOptions = {};
-  }
+  if (property === undefined) property = keyHyphenated;
+  if (rename === undefined) rename = keyHyphenated;
 
   return ({ addUtilities, e, theme, variants }) => {
-    const newUtilities = {};
-
-    for (const [name, value] of Object.entries(theme(key, {}))) {
-      const className = rename === "" ? `.${e(`${name}`)}` : `.${e(`${rename}-${name}`)}`;
-
-      newUtilities[className] = {
-        [property]: value
-      };
+    const configuration = Object.entries(theme(key, {}));
+    if (configuration.length === 0) {
+      console.warn(`the custom utility ${key} does not have any configuration in \`theme\`, so no classes can/will be generated`);
     }
-
-    const specifiedVariants = variants(key, []);
+    
+    const utilities = configuration.reduce((css, [name, value]) => ({
+      ...css,
+      [rename === "" ? `.${e(`${name}`)}` : `.${e(`${rename}-${name}`)}`]: {
+        [property]: value,
+      },
+    }), {});
 
     // This means a `variants` key in `addUtilitiesOptions` will override `specifiedVariants`!
-    addUtilities(newUtilities, { variants: specifiedVariants, ...addUtilitiesOptions });
+    addUtilities(utilities, { variants: variants(key, []), ...addUtilitiesOptions });
   };
-}
+};
 
-function multiple(...configurations) {
-  return pluginHelpers => {
-    configurations.forEach(configuration => single(configuration)(pluginHelpers));
+const multiple = (...options) =>
+  (pluginHelpers) => {
+    options.forEach((option) => single(option)(pluginHelpers));
   };
-}
 
-module.exports = function(...args) {
-  // Determine which form of arguments is being used
-  if (args.length === 0) {
+module.exports = (...options) => {
+  // Determine which form of options is being used
+  if (options.length === 0) {
     console.warn(
-      "customNative was called without any arguments; this can be fixed by passing at least one argument of the form { key, property, rename, addUtilitiesOptions }"
+      "the tailwindcss-custom-native plugin was incorrectly called without any arguments/options; this can be fixed by passing at least one argument of the form { key, property?, rename?, addUtilitiesOptions? }"
     );
-  } else if (args.length === 2 && !args[1].hasOwnProperty("key")) {
+  } else if (options.length === 2 && !options[1].hasOwnProperty("key")) {
     // The old API is being used
-    return single({ ...args[0], addUtilitiesOptions: args[1] });
+    return single({ ...options[0], addUtilitiesOptions: options[1] });
   } else {
-    return multiple(...args);
+    return multiple(...options);
   }
 };
